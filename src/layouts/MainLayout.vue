f5c8de4ebae9b41e4e4f3ce712ef3d0be09817ad
v.pecheritsya <v.pecheritsya@upstars.com>
2023-07-20 21:13:12 +0300

    Init project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="text-dark">
+          Dynamo client
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
