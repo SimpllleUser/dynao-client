9f391239da061185a6047ca4c38416c6baf2bf36
v.pecheritsya <v.pecheritsya@upstars.com>
2023-07-31 09:49:42 +0300

    feat: add entity editor

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,11 +8,27 @@
       <div class="col-4">
         <div><b>Source entity</b></div>
         <hr>
+        <Vue3JsonEditor
+          v-model="state"
+          :show-btns="true"
+          :expandedOnStart="true"
+          @json-change="onJsonChange"
+        />
+        <hr>
+        {{ entity }}
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {defineComponent, reactive, ref, toRefs} from "vue"
+import { Vue3JsonEditor } from "vue3-json-editor"
 
+const onJsonChange = (value: JSON) => {
+  console.log('value:', value)
+  entity.value = value
+}
+
+const entity = ref({})
 </script>
