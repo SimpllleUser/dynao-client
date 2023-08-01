7fc4c8aa482053df40c6721ff8a93139d7185b81
v.pecheritsya <v.pecheritsya@upstars.com>
2023-08-01 21:40:58 +0300

    feat: add start layout for properties input

--- src/components/FormConfigurator/FormInputConfigurator.vue
+++ src/components/FormConfigurator/FormInputConfigurator.vue
@@ -1,6 +1,6 @@
-<script lang="ts" setup>
+<script lang='ts' setup>
 import { useVModel } from '@vueuse/core'
-import { BackgroundColors, Colors, Option} from "boot/configurator-properties";
+import { BackgroundColors, Colors, Option } from 'boot/configurator-properties'
 import ColorSelector from 'components/UI/ColorSelector.vue'
 
 interface Configurator {
@@ -37,7 +37,7 @@ const props = defineProps<Props>()
 const colors: Array<string> = Object.keys(Colors)
 const bgColors: Array<Option> = Object.keys(BackgroundColors).map((key) => ({
   label: key,
-  value: BackgroundColors[key],
+  value: BackgroundColors[key]
 }))
 
 interface Emits {
@@ -48,35 +48,78 @@ const emit = defineEmits<Emits>()
 
 const data = useVModel(props, 'modelValue', emit)
 
+const propsStyle = {
+  filled: true,
+  labelColor: 'secondary',
+  color: 'secondary',
+}
+
 </script>
 
 <template>
   <div>
-    Form input configurator
-    <hr>
-    <div>
-      <color-selector v-model="data.color" label='Variant' customStyle='width: 11rem' />
-      <color-selector v-model="data.bgColor" label='Background color' customStyle='width: 11rem' />
-      <color-selector v-model="data.labelColor" label='Label color' customStyle='width: 11rem' />
-      <q-input v-model="data.label" label="Label"/>
-      <q-input v-model="data.shadowText" label="ShadowText"/>
-      <q-input v-model="data.prefix" label="Prefix"/>
-      <q-input v-model="data.suffix" label="Suffix"/>
-      <q-input v-model="data.maxlength" type="number" label="Max length"/>
-      <q-checkbox v-model="data.loading" label="Loading"/>
-      <q-checkbox v-model="data.stackLabel" label="StackLabel"/>
-      <q-checkbox v-model="data.counter" label="Is counter"/>
-      <q-checkbox v-model="data.clearable" label="Clearable"/>
-      <q-checkbox v-model="data.readonly" label="Readonly"/>
-      <q-checkbox v-model="data.disable" label="Disable"/>
-      <q-checkbox v-model="data.square" label="Square"/>
-      <q-checkbox v-model="data.dense" label="Dense"/>
-      <q-checkbox v-model="data.rounded" label="Rounded"/>
-      <q-checkbox v-model="data.hideBottomSpace" label="Hide bottom space"/>
-      <q-checkbox v-model="data.borderless" label="Borderless"/>
-      <q-checkbox v-model="data.outlined" label="Outlined"/>
-      <q-checkbox v-model="data.filled" label="Filled"/>
-      <q-checkbox v-model="data.dark" label="Dark"/>
+    <div class='row q-mb-md'>
+      <div class='col-6'>
+        <color-selector v-model='data.color' label='Variant' customStyle='width: 11rem' />
+      </div>
+      <div class='col-6'>
+        <color-selector v-model='data.bgColor' label='Background color' customStyle='width: 11rem' />
+      </div>
+    </div>
+    <div class='row q-mb-md'>
+      <div class='col-6'>
+        <color-selector v-model='data.labelColor' label='Label color' customStyle='width: 11rem' />
+      </div>
+    </div>
+
+    <q-input v-model='data.label' label='Label' v-bind='propsStyle' class='q-mb-md' />
+    <q-input v-model='data.shadowText' label='ShadowText' v-bind='propsStyle' class='q-mb-md' />
+    <q-input v-model='data.prefix' label='Prefix' v-bind='propsStyle' class='q-mb-md' />
+    <q-input v-model='data.suffix' label='Suffix' v-bind='propsStyle' class='q-mb-md' />
+    <q-input v-model='data.maxlength' type='number' label='Max length' v-bind='propsStyle' class='q-mb-md' />
+    <div class='row'>
+      <div class='col-6'>
+        <q-checkbox v-model='data.loading' label='Loading' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.stackLabel' label='StackLabel' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.counter' label='Is counter' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.clearable' label='Clearable' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.readonly' label='Readonly' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.disable' label='Disable' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.square' label='Square' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.dense' label='Dense' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.rounded' label='Rounded' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.hideBottomSpace' label='Hide bottom space' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.borderless' label='Borderless' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.outlined' label='Outlined' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.filled' label='Filled' v-bind='propsStyle' />
+      </div>
+      <div class='col-6'>
+        <q-checkbox v-model='data.dark' label='Dark' v-bind='propsStyle' />
+      </div>
     </div>
   </div>
 </template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,11 +2,13 @@
   <div>
     <div class='row'>
       <div class='col-8'>
-        <div><b>Form</b></div>
-        <color-selector v-model='color' />
-        <form-input v-model='text' v-bind='inputConfigurator' />
+        <div><b>Form | {{ showOption }}</b></div>
+        <form-input
+          v-model='text'
+          v-bind='inputConfigurator'
+          @focus='showOption = true'
+        />
         <hr>
-        <FormInputConfigurator v-model='inputConfigurator' />
       </div>
       <div class='col-4'>
         <div><b>Source entity</b></div>
@@ -16,16 +18,46 @@
         />
       </div>
     </div>
+
+    <q-drawer
+      side='right'
+      v-model='showOption'
+      show-if-above
+      bordered
+      width='400'
+      overlay
+      :breakpoint='500'
+    >
+      <q-scroll-area class='fit'>
+        <div class='p-y-md'>
+          <div class='row items-center'>
+            <div class='col'>
+              <q-btn icon='close' flat @click='toggleOptions()'></q-btn>
+            </div>
+            <div class='col'>
+              <b>Input properties</b>
+            </div>
+          </div>
+        <div class='q-pa-sm'>
+          <FormInputConfigurator v-model='inputConfigurator' />
+        </div>
+        </div>
+      </q-scroll-area>
+    </q-drawer>
   </div>
 </template>
 
 <script setup lang='ts'>
 import { ref } from 'vue'
+import { useToggle } from '@vueuse/core'
 import { Vue3JsonEditor } from 'vue3-json-editor'
 import FormInput from 'components/FormInputs/FormInput.vue'
 import FormInputConfigurator from 'components/FormConfigurator/FormInputConfigurator.vue'
 import { BackgroundColors, Colors } from 'boot/configurator-properties'
 import ColorSelector from 'components/UI/ColorSelector.vue'
+
+
+const [showOption, toggleOptions] = useToggle(false)
 
 const text = ref('')
 const handleChange = (value: JSON) => {
