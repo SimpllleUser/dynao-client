538ceda3bf0f3ca2156591fec0aba38b014eac1d
v.pecheritsya <v.pecheritsya@upstars.com>
2023-08-01 20:43:58 +0300

    feat: add best color selector

--- src/components/UI/ColorSelector.vue
+++ src/components/UI/ColorSelector.vue
@@ -1,21 +1,66 @@
-<script lang="ts" setup>
-import colorClasses from '../../boot/color-classes'
-import { Ref, ref } from "vue";
+<script lang='ts' setup>
+import { defineProps } from 'vue'
+import { useVModel } from '@vueuse/core'
 
-const model: Ref<string> = ref(null)
-const setColor = (classColor: string) => { model.value = classColor  }
+import colorClasses from '../../boot/color-classes'
+
+const props = withDefaults(defineProps<{
+  modelValue: string
+}>(), {
+  modelValue: 'red-5'
+})
+
+interface Emits {
+  (e: 'update:modelValue', payload: string): void
+}
+
+const emit = defineEmits<Emits>()
+
+const data = useVModel(props, 'modelValue', emit)
+const setColor = (classColor: string) => {
+  data.value = classColor
+}
 </script>
 
 <template>
-    <div>
-      <q-select filled v-model="model" :options="colorClasses" label="Color" autocomplete >
-        <template #option="{ opt }">
-          <div class="cursor-pointer" :class="`bg-${opt} text-${opt}`" @click="setColor(opt)">
-            -
+  <div>
+    <q-btn-dropdown
+      split
+      :color='data'
+      push
+      no-caps
+      class='text-black'
+      style='width: 6rem'
+    >
+      <template v-slot:label>
+        <div class='row items-center no-wrap'>
+          <div class='text-center text-black'>
+            color
           </div>
-        </template>
-      </q-select>
-      {{ model }}</div>
+        </div>
+      </template>
+
+      <div
+        style='
+          width: 100%;
+          max-width: 240px;
+          display: flex;
+          flex-wrap: wrap;'
+      >
+        <div
+          v-for='color in colorClasses'
+          :key='color'
+          clickable
+          v-close-popup
+          @click='setColor(color)'
+          class='cursor-pointer'
+          :class='`bg-${color}`'
+          style='width: 1rem; height: 1rem'
+        >
+        </div>
+      </div>
+    </q-btn-dropdown>
+  </div>
 </template>
 
 <style scoped></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,37 +1,38 @@
 <template>
   <div>
-    <div class="row">
-      <div class="col-8">
+    <div class='row'>
+      <div class='col-8'>
         <div><b>Form</b></div>
-        <color-selector />
-        <form-input v-model="text" v-bind="inputConfigurator" />
+        <color-selector v-model='color' />
+        <form-input v-model='text' v-bind='inputConfigurator' />
         <hr>
-        <FormInputConfigurator v-model="inputConfigurator" />
+        <FormInputConfigurator v-model='inputConfigurator' />
       </div>
-      <div class="col-4">
+      <div class='col-4'>
         <div><b>Source entity</b></div>
         <hr>
         <Vue3JsonEditor
-          @json-change="handleChange"
+          @json-change='handleChange'
         />
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue"
-import { Vue3JsonEditor } from "vue3-json-editor"
-import FormInput from "components/FormInputs/FormInput.vue";
-import FormInputConfigurator from "components/FormConfigurator/FormInputConfigurator.vue";
-import { BackgroundColors, Colors} from "boot/configurator-properties";
-import ColorSelector from "components/UI/ColorSelector.vue";
+<script setup lang='ts'>
+import { ref } from 'vue'
+import { Vue3JsonEditor } from 'vue3-json-editor'
+import FormInput from 'components/FormInputs/FormInput.vue'
+import FormInputConfigurator from 'components/FormConfigurator/FormInputConfigurator.vue'
+import { BackgroundColors, Colors } from 'boot/configurator-properties'
+import ColorSelector from 'components/UI/ColorSelector.vue'
 
 const text = ref('')
 const handleChange = (value: JSON) => {
   entity.value = value
 }
 
+const color = ref()
 const entity = ref({})
 const inputConfigurator = ref({
   color: Colors.primary,
