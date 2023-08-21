fdd415ad863f046b66a23d81aa926d21a91413b8
v.pecheritsya <v.pecheritsya@upstars.com>
2023-08-21 05:37:06 +0300

    feat(Input): add general props

--- src/modules/ConfiguratorComponents/config.ts
+++ src/modules/ConfiguratorComponents/config.ts
@@ -3,16 +3,43 @@ import { InputComponentTypes } from 'src/modules/UIParser/types'
 
 export const commonPropsConfigurator = [
     getRow({ class: 'w-full' }, [
-        {
-            type: InputComponentTypes.Input,
-            props: {
-                model: 'label',
-                label: 'Label',
-                placeholder: 'Input label',
-                filled: true,
-                class: 'full-width',
+        getCol({}, [
+            {
+                type: InputComponentTypes.Select,
+                props: {
+                    model: 'type',
+                    label: 'Type',
+                    placeholder: 'Type input',
+                    filled: true,
+                    class: 'full-width',
+                    options: [
+                        'text',
+                        'password',
+                        'textarea',
+                        'email',
+                        'search',
+                        'tel',
+                        'file',
+                        'number',
+                        'url',
+                        'time',
+                        'date',
+                    ],
+                },
             },
-        },
+        ]),
+        getCol({}, [
+            {
+                type: InputComponentTypes.Input,
+                props: {
+                    model: 'label',
+                    label: 'Label',
+                    placeholder: 'Input label',
+                    filled: true,
+                    class: 'full-width',
+                },
+            },
+        ]),
     ]),
     getRow({ class: 'w-full q-pt-sm' }, [
         {
@@ -60,6 +87,64 @@ export const commonPropsConfigurator = [
                 props: {
                     model: 'clearable',
                     label: 'Clearable',
+                },
+            },
+        ]),
+    ]),
+    getRow({ class: 'w-full q-mt-sm' }, [
+        getCol({}, [
+            {
+                type: InputComponentTypes.Checkbox,
+                props: {
+                    model: 'standout',
+                    label: 'Standout',
+                },
+            },
+        ]),
+        getCol({}, [
+            {
+                type: InputComponentTypes.Checkbox,
+                props: {
+                    model: 'square',
+                    label: 'Square',
+                },
+            },
+        ]),
+        getCol({}, [
+            {
+                type: InputComponentTypes.Checkbox,
+                props: {
+                    model: 'rounded',
+                    label: 'Rounded',
+                },
+            },
+        ]),
+        getCol({}, [
+            {
+                type: InputComponentTypes.Checkbox,
+                props: {
+                    model: 'borderless',
+                    label: 'Borderless',
+                },
+            },
+        ]),
+    ]),
+    getRow({ class: 'w-full q-mt-sm' }, [
+        getCol({}, [
+            {
+                type: InputComponentTypes.ColorSelector,
+                props: {
+                    model: 'color',
+                    label: 'Color',
+                },
+            },
+        ]),
+        getCol({}, [
+            {
+                type: InputComponentTypes.ColorSelector,
+                props: {
+                    model: 'bg-color',
+                    label: 'Background color',
                 },
             },
         ]),
